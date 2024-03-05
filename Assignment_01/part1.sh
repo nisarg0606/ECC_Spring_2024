16ffd7033a499fa4b6bd06613b48a7fa4a3b1324
Nisarg Shah <nisarg0606@gmail.com>
2024-03-05 03:03:38 -0500

    part 1 and 2 done

---
+++ Assignment_01/part1.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Define HDFS input and output paths
+hdfs_input="/Assignment_01/Input/access.log"
+hdfs_output="/Assignment_01/Part_01/output/$(date +'%d-%B-%Y-%H-%M-%S')"
+
+# Check if the output directory exists, delete it if it does
+hdfs dfs -test -d $hdfs_output
+if [ $? -eq 0 ]; then
+    echo "Deleting existing output directory: $hdfs_output"
+    hdfs dfs -rm -r $hdfs_output
+fi
+
+# Ask the user for map and reduce scripts
+echo "Which mapper and reducer scripts would you like to use?"
+echo "Enter 1 for mapper1.py and reducer1.py"
+echo "Enter 2 for mapper2.py and reducer2.py"
+read choice
+
+# Set mapper and reducer scripts based on user input
+if [ $choice -eq 1 ]; then
+    mapper_script="/home/hadoop/Assignment_01/Part_01/mapper1.py"
+    reducer_script="/home/hadoop/Assignment_01/Part_01/reducer1.py"
+elif [ $choice -eq 2 ]; then
+    mapper_script="/home/hadoop/Assignment_01/Part_01/mapper2.py"
+    reducer_script="/home/hadoop/Assignment_01/Part_01/reducer2.py"
+else
+    echo "Invalid choice."
+    exit 1
+fi
+
+# Run the Hadoop streaming job
+hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-3.3.6.jar \
+    -mapper "/usr/bin/python3 $mapper_script" \
+    -reducer "/usr/bin/python3 $reducer_script" \
+    -input $hdfs_input \
+    -output $hdfs_output
+
+echo "Job completed. Output stored in: $hdfs_output"
--- Assignment_01/part2.sh
+++ Assignment_01/part2.sh
@@ -5,8 +5,8 @@ read -p "Enter the start time (format HH): " start_hour
 read -p "Enter the end time (format HH): " end_hour
 
 # Define HDFS input and output paths
-hdfs_input="/assignment01/input/access.log"
-hdfs_output="/assignment01/part2/output/$(date +%s)"
+hdfs_input="/Assignment_01/Input/access.log"
+hdfs_output="/Assignment_01/Part_02/output/$(date +'%d-%B-%Y-%H-%M-%S')"
 
 # Check if the output directory exists, delete it if it does
 hdfs dfs -test -d $hdfs_output
@@ -16,11 +16,11 @@ if [ $? -eq 0 ]; then
 fi
 
 # Run the Hadoop streaming job
+#-file /home/hadoop/Assignment_01/Part_02/mapper2.py \
+#-file /home/hadoop/Assignment_01/Part_02/reducer2.py \
 hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-3.3.6.jar \
-    -file /home/hadoop/assignment01/part2/mapper.py \
-    -mapper "/usr/bin/python3 /home/hadoop/assignment01/part2/mapper.py" \
-    -file /home/hadoop/assignment01/part2/reducer.py \
-    -reducer "/usr/bin/python3 /home/hadoop/assignment01/part2/reducer.py" \
+    -mapper "/usr/bin/python3 /home/hadoop/Assignment_01/Part_02/mapper2.py" \
+    -reducer "/usr/bin/python3 /home/hadoop/Assignment_01/Part_02/reducer2.py" \
     -input $hdfs_input \
     -cmdenv TIMEFRAME="$start_hour-$end_hour" \
     -output $hdfs_output
