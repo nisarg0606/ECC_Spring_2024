61c1c6f1ec6242c6aa5cc81ebb49bfe85a312154
Nisarg Shah <nisarg0606@gmail.com>
2024-03-05 01:04:10 -0500

    removed java

--- assignment01/python_run_job_p1.sh
+++ assignment01/python_run_job_p1.sh
@@ -3,7 +3,7 @@
 # Define HDFS input and output paths
 hdfs_input="/assignment01/input/access.log"
 #keep the output directory dynamic
-hdfs_output="/assignment01/python/part1/output/$(date +%s)"
+hdfs_output="/assignment01/part1/output/$(date +%s)"
 
 # Check if the output directory exists, delete it if it does
 hdfs dfs -test -d $hdfs_output
@@ -14,10 +14,10 @@ fi
 
 # Run the Hadoop streaming job
 hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-3.3.6.jar \
-    -file /home/hadoop/assignment01/python/part1/mapper2.py \
-    -mapper "/usr/bin/python3 /home/hadoop/assignment01/python/part1/mapper2.py" \
-    -file /home/hadoop/assignment01/python/part1/reducer2.py \
-    -reducer "/usr/bin/python3 /home/hadoop/assignment01/python/part1/reducer2.py" \
+    -file /home/hadoop/assignment01/part1/mapper2.py \
+    -mapper "/usr/bin/python3 /home/hadoop/assignment01/part1/mapper2.py" \
+    -file /home/hadoop/assignment01/part1/reducer2.py \
+    -reducer "/usr/bin/python3 /home/hadoop/assignment01/part1/reducer2.py" \
     -input $hdfs_input \
     -output $hdfs_output
 
--- assignment01/python_run_job_p2.sh
+++ assignment01/python_run_job_p2.sh
@@ -6,7 +6,7 @@ read -p "Enter the end time (format HH): " end_hour
 
 # Define HDFS input and output paths
 hdfs_input="/assignment01/input/access.log"
-hdfs_output="/assignment01/python/part2/output/$(date +%s)"
+hdfs_output="/assignment01/part2/output/$(date +%s)"
 
 # Check if the output directory exists, delete it if it does
 hdfs dfs -test -d $hdfs_output
@@ -17,10 +17,10 @@ fi
 
 # Run the Hadoop streaming job
 hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-3.3.6.jar \
-    -file /home/hadoop/assignment01/python/part2/mapper.py \
-    -mapper "/usr/bin/python3 /home/hadoop/assignment01/python/part2/mapper.py" \
-    -file /home/hadoop/assignment01/python/part2/reducer.py \
-    -reducer "/usr/bin/python3 /home/hadoop/assignment01/python/part2/reducer.py" \
+    -file /home/hadoop/assignment01/part2/mapper.py \
+    -mapper "/usr/bin/python3 /home/hadoop/assignment01/part2/mapper.py" \
+    -file /home/hadoop/assignment01/part2/reducer.py \
+    -reducer "/usr/bin/python3 /home/hadoop/assignment01/part2/reducer.py" \
     -input $hdfs_input \
     -cmdenv TIMEFRAME="$start_hour-$end_hour" \
     -output $hdfs_output
